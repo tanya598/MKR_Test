907f5651a6cf4f3f8ecff9b3a942a8a59f3e1e33
tanya598 <obrysnik.tanya@gmail.com>
2023-03-30 14:05:53 +0300

    Опис коду  функція перебирає кожен рядок за рядком і виконує деяку обробку кожного рядка. Рядок містить три значення, розділені комами: назва країни, рік і чисельність населення країни в цьому році. Метод strip використовується для видалення будь-яких пробілів із початку та кінця рядка, а метод split використовується для поділу рядка на три значення.
    Значення року та населення перетворюються з рядків на цілі числа за допомогою функції int. Потім функція перевіряє, чи країна вже є в словнику даних. Якщо ні, він додає країну як ключ до даних із порожнім словником як її значення.
    
    Потім функція перевіряє, чи ключ last_year уже є в словнику для поточної країни. Якщо так, він обчислює зміну чисельності населення як різницю між чисельністю населення поточного року та чисельністю населення за минулий рік і додає цю зміну чисельності населення до словника даних для поточної країни та року.
    
    Функція оновлює ключі last_population і last_year у словнику даних для поточної країни поточним роком і значеннями населення.
    
     функція calc_population_change повертає словник даних, що містить дані про зміну населення для кожної країни та року.

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ def calc_population_change(filename):
             country, year, population = line.strip().split(', ')
             year, population = int(year), int(population)
             if country not in data:
+                data[country] = {}
+            if 'last_year' in data[country]:
+                population_change = population - data[country]['last_population']
+                data[country][year] = population_change
+            data[country]['last_population'] = population
+            data[country]['last_year'] = year
+        return data
